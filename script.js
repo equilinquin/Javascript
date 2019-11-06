de8491944eb3f155ba2f57dfd767560761966f61
Elizabeth Garcia-Quilinquin <equilinquin@gmail.com>
2019-11-05 18:42:05 -0800

    iniial update for java

---
+++ script.js
@@ -0,0 +1,21 @@
+//onclick start prompts
+    
+
+//how many characters user wants
+//do they want to use special char
+//do they want to use numbers
+//do they want to use lowercase.
+//do they want to use uppercase.
+//display password.
+//set variables.
+
+
+var pass = {
+    sym = ["!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]
+    num = ["1234567890"]
+    low = ["abclefghijklmnopqrstuvwxyz"]
+    cap = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+}
+var user = ''
+
+//generate password.
